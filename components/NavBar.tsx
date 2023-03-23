b2216b403753086ec86bd266c0ed73f83f0d98ad
hxncodes <hxncodes@gmail.com>
2023-03-23 09:12:02 +0500

    Navbar added

---
+++ components/NavBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <nav className="navbar bg-primary" >
+            <div className="container">
+                <a className="navbar-brand" href="#" >
+                    hxnCodes
+                </a>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import React from 'react'
+import NavBar from '../components/NavBar'
 import Alerts from '../components/Alerts'
 
 const App = () => {
   return (
     <div>
+      <NavBar />
       <Alerts />
     </div>
   )
