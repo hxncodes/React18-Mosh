c52c256a101ea45c524a7c4270a1763133daa08c
hxncodes <hxncodes@gmail.com>
2023-03-23 08:57:24 +0500

    Starter Template with React18 and Bootstrap 5.2.3

---
+++ components/Alerts.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Alerts = () => {
+  return (
+    <div><div className="alert alert-primary" role="alert">
+      A simple primary alert—check it out!
+    </div>
+      <button type="button" className="btn btn-primary">Primary</button></div>
+  )
+}
+
+export default Alerts
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from 'react'
+import Alerts from '../components/Alerts'
+
+const App = () => {
+  return (
+    <div>
+      <Alerts />
+    </div>
+  )
+}
+
+export default App
