d999b457551c751b6238e2ce7a9faa95cc261447
hxncodes <hxncodes@gmail.com>
2023-03-23 11:00:48 +0500

    Conditional Rendering, Handling Events, Passing Props

---
+++ components/ListGroup.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+interface Props {
+    items: String[];
+    heading: String;
+    onSelectItem: (item: String) => void
+}
+
+const ListGroup = ({ items, heading, onSelectItem }: Props) => {
+
+
+    let [selectedIndex, setselectedIndex] = useState(-1)
+
+    return (
+        <>
+            <h1>{heading}</h1>
+            {items.length === 0 && <p>No items found</p>}
+            <ul className='list-group'>
+                {items.map((item, index) => (
+                    <li
+                        className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+                        key={item}
+                        onClick={() => {
+                            setselectedIndex(index);
+                            onSelectItem(item)
+                        }}
+                    >
+                        {item}
+                    </li>
+                ))}
+            </ul >
+        </>
+    )
+}
+
+export default ListGroup
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import './App.css'
 import React from 'react'
 import NavBar from '../components/NavBar'
+import ListGroup from '../components/ListGroup'
 import Alerts from '../components/Alerts'
 
 const App = () => {
+  let items = ['New York', 'San Fransicso', 'Tokyo', 'London', 'Paris']
+  const handleSelectItem = (item: String) => {
+    console.log(item);
+
+  }
   return (
     <div>
       <NavBar />
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
       <Alerts />
     </div>
   )
