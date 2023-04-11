922640e9ee7b32a2792ff58482c748611a496ca9
Hasnat Rana <hxncodes@gmail.com>
2023-04-11 13:57:35 +0500

    Form Submitting through react-hook-form@7.43

--- forms/FormStateHook.tsx
+++ forms/FormStateHook.tsx
@@ -1,26 +1,50 @@
-import React, { FormEvent, useState } from "react";
+import { FormEvent, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 const FormStateHook = () => {
-  // creating Person object with state hook
-  const [person, setPerson] = useState({ name: '', age: '' })
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: FieldValues) => console.log(data);
 
-  const handleSubmit = (event:FormEvent) => { event?.preventDefault(); console.log(person);}
- 
+  // creating Person object with state hook
+  // const [person, setPerson] = useState({ name: "", age: "" });
+
+  // const handleSubmit = (event: FormEvent) => {
+  //   event?.preventDefault();
+  //   console.log(person);
+  // };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h3>Form Submitting through useState Hook</h3>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h3>Form Submitting through react-hook-form@7.43</h3>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input onChange={(event) => setPerson({ ...person, name: event.target.value })}  value={person.name} id="name" type="text" className="form-control" />
+        <input
+          // onChange={(event) =>
+          //   setPerson({ ...person, name: event.target.value })
+          // }
+          // value={person.name}
+          {...register("name")}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input onChange={(event) => setPerson({...person, age: parseInt(event.target.value)})} value={person.age} id="age" type="number" className="form-control" />
+        <input
+          // onChange={(event) =>
+          //   setPerson({ ...person, age: parseInt(event.target.value) })
+          // }
+          // value={person.age}
+          {...register("age")}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
