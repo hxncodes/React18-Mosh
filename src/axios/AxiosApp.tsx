c4a832b615f017b04f90043eea795ecc2c1cac58
Hasnat Rana <hxncodes@gmail.com>
2023-04-13 21:37:59 +0500

    Axios- Cancelling a fetch request

--- src/axios/AxiosApp.tsx
+++ src/axios/AxiosApp.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 import React, { useEffect, useState } from "react";
 interface User {
   id: number;
@@ -8,10 +8,19 @@ const AxiosApp = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
   useEffect(() => {
+    const controller = new AbortController();
+
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
       .then((res) => setUsers(res.data))
-      .catch((err) => setError(err.message));
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort();
   }, []);
   return (
     <>
