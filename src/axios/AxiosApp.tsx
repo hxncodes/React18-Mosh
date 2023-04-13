fe347ebc60c903aef0b327ee2c2cc83af03b1385
Hasnat Rana <hxncodes@gmail.com>
2023-04-13 12:47:00 +0500

    Axios- Fetch with Async, Await method

--- src/axios/AxiosApp.tsx
+++ src/axios/AxiosApp.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import React, { useEffect, useState } from "react";
 
 interface User {
@@ -8,17 +8,29 @@ interface User {
 
 const AxiosApp = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
   useEffect(() => {
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get<User[]>(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        setUsers(res.data);
+      } catch (err) {
+        setError((err as AxiosError).message);
+      }
+    };
+    fetchUsers();
   }, []);
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <p className="text-danger">{error}</p>
+    </>
   );
 };
 
