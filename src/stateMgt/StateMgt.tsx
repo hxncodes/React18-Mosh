77c67c15f77b12b3afa2ec79b4aea8812f899aab
Hasnat Rana <hxncodes@gmail.com>
2023-04-13 21:20:00 +0500

    Project folders efactored

---
+++ src/stateMgt/StateMgt.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import ListGroup from "./components/ListGroup";
+import Alerts from "./components/Alerts";
+import Button from "./components/Button";
+import Btn from "./components/Btn";
+import ExpandableComponent from "./components/ExpandableComponent";
+
+const App = () => {
+  let items = ["New York", "San Fransicso", "Tokyo", "London", "Paris"];
+  const handleSelectItem = (item: String) => {
+    console.log(item);
+  };
+
+  const [alertVisibility, setAlertVisibility] = useState(false);
+  return (
+    <div>
+      {alertVisibility && (
+        <Alerts onClose={() => setAlertVisibility(false)}>
+          This is Alert message..!
+        </Alerts>
+      )}
+
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+
+      <Button color="warning" onClick={() => setAlertVisibility(true)}>
+        Get Alerts
+      </Button>
+      <ExpandableComponent maxChars={50}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
+        delectus veritatis eligendi repudiandae velit, illo hic aliquam,
+        assumenda alias cumque officia dignissimos voluptates. Obcaecati esse
+        numquam reiciendis quas iusto dolorem nobis vitae, ea deserunt illum
+        praesentium eaque quod, minima exercitationem itaque ad? Aspernatur
+        fugit, incidunt voluptatem, commodi tempora repudiandae animi ad, cum et
+        voluptate eius. Unde et ullam voluptatibus distinctio doloremque harum
+        tempora reprehenderit quos! Voluptatibus, tenetur officiis! Aut et qui
+        eum non, recusandae magni distinctio minus fugit nulla! Sit fugiat iste
+        quo ipsa laboriosam ratione magnam, neque corporis itaque cupiditate
+        sapiente cum voluptate reprehenderit eaque facere officiis delectus
+        ipsum.
+      </ExpandableComponent>
+      <Btn color="danger" onBtnClick={() => setAlertVisibility(true)}>
+        Child
+      </Btn>
+    </div>
+  );
+};
+
+export default App;
