7dea99fef74b63b5a4cb5ae1e650e767c89e98c6
Hasnat Rana <hxncodes@gmail.com>
2023-04-13 12:48:04 +0500

    Axios- Fetch simple method

--- src/axios/AxiosApp.tsx
+++ src/axios/AxiosApp.tsx
@@ -1,26 +1,17 @@
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import React, { useEffect, useState } from "react";
-
 interface User {
   id: number;
   name: string;
 }
-
 const AxiosApp = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const res = await axios.get<User[]>(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        setUsers(res.data);
-      } catch (err) {
-        setError((err as AxiosError).message);
-      }
-    };
-    fetchUsers();
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data))
+      .catch((err) => setError(err.message));
   }, []);
   return (
     <>
@@ -33,5 +24,4 @@ const AxiosApp = () => {
     </>
   );
 };
-
 export default AxiosApp;
