9ab0158421421a46c3e241cc0b86c907570e833f
Hasnat Rana <hxncodes@gmail.com>
2023-04-14 22:23:16 +0500

    Created a custom Hook for data fetching

--- src/axios/AxiosApp.tsx
+++ src/axios/AxiosApp.tsx
@@ -1,27 +1,10 @@
 import { useEffect, useState } from "react";
 import { CanceledError } from "./services/api-client";
 import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 
 const AxiosApp = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    const { request, cancel } = userService.getAll<User>();
-    request
-      .then((res) => {
-        setUsers(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-    return () => cancel();
-  }, []);
+  const { users, error, isLoading, setUsers, setError } = useUsers();
 
   // Delete User
   const deleteUser = (user: User) => {
